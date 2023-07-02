272b495a5bcc6cac8d9cdeb76147d9737ba2f752
Kartik <kartiksindura@Kartik.local>
2023-07-03 03:00:21 +0530

    noSSR

--- src/app/page.js
+++ src/app/page.js
@@ -2,21 +2,24 @@ import Image from "next/image";
 import Item from "./components/item";
 import Item2 from "./components/item2";
 import Item3 from "./components/item3";
+import NoSSR from "./components/NoSSR";
 
 export default function Home() {
   return (
     <>
       <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5">
-        {[...Array(10)].map((e, i) => {
-          let x = Math.random()
-          if (x < 0.33) {
-            return <Item />;
-          } else if (x > 0.33 && x < 0.66){
-            return <Item2 />;
-          } else {
-            return <Item3 />;
-          }
-        })}
+        <NoSSR>
+          {[...Array(10)].map((e, i) => {
+            let x = Math.random();
+            if (x < 0.33) {
+              return <Item />;
+            } else if (x > 0.33 && x < 0.66) {
+              return <Item2 />;
+            } else {
+              return <Item3 />;
+            }
+          })}
+        </NoSSR>
       </div>
     </>
   );
