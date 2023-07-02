070c1ca3cdfd5d8cf7f68e580ca21c6cc196d2e1
Kartik <kartiksindura@Kartik.local>
2023-07-03 02:32:11 +0530

    test

---
+++ src/app/components/item3.js
@@ -0,0 +1,16 @@
+
+export default function Item3() {
+  return (
+    <div className="grid grid-cols-3 gap-x-2 gap-y-2 grid-flow-row-dense mx-2 my-2">
+      <input type="checkbox" className="border-l-violet-600 border-2 btn h-full shadow-xl min-h-[50px] col-span-2" />
+      <input type="checkbox" className="border-l-violet-600 border-2 btn h-full shadow-xl min-h-[50px] col-span-2 row-span-3" />
+      <input type="checkbox" className="border-l-violet-600 border-2 btn h-full shadow-xl min-h-[50px]" />
+      <input type="checkbox" className="border-l-violet-600 border-2 btn h-full shadow-xl min-h-[50px]  col-span-3" />
+      <input type="checkbox" className="border-l-violet-600 border-2 btn h-full shadow-xl min-h-[50px]" />
+      <input type="checkbox" className="border-l-violet-600 border-2 btn h-full shadow-xl min-h-[50px]" />
+      <input type="checkbox" className="border-l-violet-600 border-2 btn h-full shadow-xl min-h-[50px] row-span-2" />
+      <input type="checkbox" className="border-l-violet-600 border-2 btn h-full shadow-xl min-h-[50px] col-span-2 row-span-2 " />
+      <input type="checkbox" className="border-l-violet-600 border-2 btn h-full shadow-xl min-h-[50px] " />
+    </div>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Item from "./components/item";
+import Item2 from "./components/item2";
+import Item3 from "./components/item3";
+
+export default function Home() {
+  return (
+    <>
+      <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5">
+        {[...Array(10)].map((e, i) => {
+          let x = Math.random()
+          if (x < 0.33) {
+            return <Item />;
+          } else if (x > 0.33 && x < 0.66){
+            return <Item2 />;
+          } else {
+            return <Item3 />;
+          }
+        })}
+      </div>
+    </>
+  );
+}
