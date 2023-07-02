0bbcacf4ff9c11e127b41884e4bba2002206b934
Kartik <kartiksindura@Kartik.local>
2023-07-03 03:22:55 +0530

    useMemo

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,17 @@
+"use client";
 import Image from "next/image";
 import Item from "./components/item";
 import Item2 from "./components/item2";
 import Item3 from "./components/item3";
 import NoSSR from "./components/NoSSR";
+import { useEffect, useState, useMemo } from "react";
 
 export default function Home() {
   return (
     <>
-      <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5">
-        <NoSSR>
+        <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5">
           {[...Array(10)].map((e, i) => {
-            let x = Math.random();
+             const x = useMemo(() => (Math.random()), []);
             if (x < 0.33) {
               return <Item />;
             } else if (x > 0.33 && x < 0.66) {
@@ -19,8 +20,7 @@ export default function Home() {
               return <Item3 />;
             }
           })}
-        </NoSSR>
-      </div>
+        </div>
     </>
   );
 }
